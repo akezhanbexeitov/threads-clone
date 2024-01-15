6b19d3cda1d6f72076c6a31deeaf3a4047edfdb6
Akezhan <akidulatini@gmail.com>
2024-01-15 19:02:25 +0600

    Add assets

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function TopBar() {
-  return <h1>Top bar</h1>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="Logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+    </nav>
+  );
 }
 
 export default TopBar;
