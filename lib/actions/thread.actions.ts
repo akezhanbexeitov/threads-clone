f7527ea05f35757f8661e3ef73b3a00f032d6061
Akezhan <akidulatini@gmail.com>
2024-01-19 01:01:41 +0600

    Create ThreadCard, show threads on main page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -36,3 +36,32 @@ export const createThread = async ({
         throw new Error(`Error creating thread: ${error.message}`)
     }
 }
+
+export const fetchThreads = async (pageNumber = 1, pageSize = 20) => {
+    await connectToDB()
+
+    // Calculate the number of threads to skip
+    const skipAmount = (pageNumber - 1) * pageSize
+
+    const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: -1 })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: 'User' })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: 'User',
+                select: '_id name parentId image'
+            }
+        })
+    
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+    const threads = await threadsQuery.exec()
+
+    const isNext = totalPostsCount > skipAmount + threads.length
+
+    return { threads, isNext }
+}
