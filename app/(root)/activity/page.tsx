ccc52d99b46dde73cc78fa3701e11431e4ba12a2
Akezhan <akidulatini@gmail.com>
2024-01-20 20:04:11 +0600

    Complete activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,7 +1,54 @@
+import { fetchUser, getNotifications } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const notifications = await getNotifications(userInfo._id);
+
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
+
+      <section className="mt-10 flex flex-col gap-5">
+        {notifications.length > 0 ? (
+          <>
+            {notifications.map((notification) => (
+              <Link
+                key={notification._id}
+                href={`/thread/${notification.parentId}`}
+              >
+                <article className="activity-card">
+                  <Image
+                    src={notification.author.image}
+                    alt="Profile picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {notification.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">
+            No notifications yet
+          </p>
+        )}
+      </section>
     </section>
   );
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -137,3 +137,27 @@ export const fetchUsers = async ({
         throw new Error(`Failed to fetch users: ${error.message}`)
     }
 }
+
+export const getNotifications = async (userId: string) => {
+    try {
+        await connectToDB()
+
+        const userThreads = await Thread.find({ author: userId })
+        const childThreadIDs = userThreads.reduce((acc, thread) => {
+            return acc.concat(thread.children)
+        }, [])
+
+        const replies = await Thread.find({
+            _id: { $in: childThreadIDs },
+            author: { $ne: userId },
+        }).populate({
+            path: "author",
+            model: User,
+            select: "name image _id",
+        })
+        
+        return replies
+    } catch (error: any) {
+        throw new Error(`Failed to fetch notifications: ${error.message}`)
+    }
+}
