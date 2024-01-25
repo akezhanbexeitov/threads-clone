b0bb95d277be0867b67f1f8150d9453ea93e5024
Akezhan <akidulatini@gmail.com>
2024-01-26 01:33:22 +0600

    Add comments with images of authors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,18 +3,18 @@ import { fetchThreads } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
-  const threads = await fetchThreads(1, 30);
+  const { threads } = await fetchThreads(1, 30);
   const user = await currentUser();
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>;
       <section className="mt-9 flex flex-col gap-10">
-        {threads.threads.length === 0 ? (
+        {threads.length === 0 ? (
           <p className="no-result">No threads found</p>
         ) : (
           <>
-            {threads.threads.map((thread) => (
+            {threads.map((thread) => (
               <ThreadCard
                 key={thread._id}
                 id={thread._id}
